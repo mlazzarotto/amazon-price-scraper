e8b41b73ef6f57bf36a85cb00193eac6875f7543
Marco Lazzarotto <mlazzarotto91@gmail.com>
2019-11-28 10:07:28 +0100

    Implemented read email settings from JSON, minor improvements

--- amazon_price_tracker.py
+++ amazon_price_tracker.py
@@ -18,20 +18,22 @@ class AmazonItem:
         self.url = url
         self.desired_price = desired_price
 
+
 def get_user_agent():
     # Function that reads user agents from a json file
     try:
         with open(os.path.join(sys.path[0], 'user_agents.json')) as json_file:
             user_agents = json.load(json_file)
     except OSError as e:
-        print('Cannot read the JSON file \n', e)
+        print('Cannot read the user_agensts.json file \n', e)
         sys.exit()
 
     return (user_agents[randrange(len(user_agents))]['useragent'])
 
+
 def check_price(amazon_item):
     # Function that checks price
-    time.sleep(randrange(0,15))
+    time.sleep(randrange(15))
 
     headers = {"User-agent": get_user_agent()}
 
@@ -48,9 +50,10 @@ def check_price(amazon_item):
         converted_price = float(price[0:6].replace(',', '.'))
         if (converted_price < amazon_item.desired_price):
             send_email(amazon_item, converted_price)
-        
+
         print(amazon_item.name)
         print(converted_price)
+
 
 def send_email(amazon_item, price):
 
@@ -58,40 +61,61 @@ def send_email(amazon_item, price):
     # @name = name of the item
     # @price = price of the item
 
-    server = smtplib.SMTP('smtp.gmail.com', 587)
+    # Trying to read JSON file
+    try:
+        with open(os.path.join(sys.path[0], 'settings.json')) as json_file:
+            settings = json.load(json_file)
+    except OSError as e:
+        print('Cannot read the settings.json file \n', e)
+        sys.exit()
+
+    # Email server settings
+    email_settings = settings['email_settings']
+    sender = email_settings[0]['sender']
+    recipient = email_settings[0]['recipient']
+    smtp_server = email_settings[0]['smtp_server']
+    smtp_port = email_settings[0]['smtp_port']
+    smtp_username = email_settings[0]['smtp_username']
+    smtp_password = email_settings[0]['smtp_password']
+
+    server = smtplib.SMTP(smtp_server, smtp_port)
     server.ehlo()
     server.starttls()
     server.ehlo()
+    server.login(smtp_username, smtp_password)
 
-    server.login('***REMOVED***', '***REMOVED***')
-
+    # Body and subject of the email
     subject = 'The price for {} is {}'.format(amazon_item.name, price)
     body = 'Check it now on Amazon at link \n {}'.format(amazon_item.url)
-
     msg = "Subject: {} \n\n {}".format(subject, body)
 
     server.sendmail(
         # From
-        '***REMOVED***',
+        sender,
         # To
-        '***REMOVED***',
+        recipient,
         msg
     )
     print('Hey! I\'ve just sent the email')
     server.quit()
 
 
+# List of Amazon items to check
 items_list = []
-items_list.append(AmazonItem('MI A3', 'https://www.amazon.it/Xiaomi-Mi-4GB-64GB-Version/dp/B07VD3JH2C', 155))
-items_list.append(AmazonItem('Striscia LED Wifi BRLTX', 'https://www.amazon.it/BRTLX-Striscia-Impermeabile-intelligente-controllato/dp/B07KHX5T58', 23))
-items_list.append(AmazonItem('Striscia LED Wifi Onforu', 'https://www.amazon.it/Onforu-Compatibile-Telecomando-Alimentatore-Illuminazione/dp/B07S15QLT7', 24))
-items_list.append(AmazonItem('Striscia LED Wifi Bawoo', 'https://www.amazon.it/Impermeabile-Illuminazione-Bawoo-Assistant-Telecomando/dp/B078SNWRS4', 24))
+items_list.append(AmazonItem(
+    'MI A3', 'https://www.amazon.it/Xiaomi-Mi-4GB-64GB-Version/dp/B07VD3JH2C', 1550))
+items_list.append(AmazonItem('Striscia LED Wifi BRLTX',
+                             'https://www.amazon.it/BRTLX-Striscia-Impermeabile-intelligente-controllato/dp/B07KHX5T58', 230))
+items_list.append(AmazonItem('Striscia LED Wifi Onforu',
+                             'https://www.amazon.it/Onforu-Compatibile-Telecomando-Alimentatore-Illuminazione/dp/B07S15QLT7', 240))
+items_list.append(AmazonItem('Striscia LED Wifi Bawoo',
+                             'https://www.amazon.it/Impermeabile-Illuminazione-Bawoo-Assistant-Telecomando/dp/B078SNWRS4', 240))
 
 
 while True:
     try:
-        for item in items_list: 
+        for item in items_list:
             check_price(item)
     except:
         pass
-    time.sleep(5 * 60)
+    time.sleep(5 * 6)

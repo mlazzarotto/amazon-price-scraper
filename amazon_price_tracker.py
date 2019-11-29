5f681da309c0e7937503b14d0238abb18d51a3a3
Marco Lazzarotto <mlazzarotto91@gmail.com>
2019-11-29 19:14:45 +0100

    Added more comments

--- amazon_price_tracker.py
+++ amazon_price_tracker.py
@@ -19,7 +19,10 @@ class AmazonItem:
 
 
 def get_user_agent():
-    # Function that reads user agents from a json file
+    """ Function that reads user agents from a json file
+    The file name be 'user_agents.json'
+    To get updated user agenst, visit https://techblog.willshouse.com/2012/01/03/most-common-user-agents
+    """
     try:
         with open(os.path.join(sys.path[0], 'user_agents.json')) as json_file:
             user_agents = json.load(json_file)
@@ -31,7 +34,9 @@ def get_user_agent():
 
 
 def check_price(amazon_item):
-    # Function that checks price
+    """ Function that checks price
+    Takes an AmazonItem instance as parameter
+    """
     time.sleep(randrange(15))
 
     headers = {"User-agent": get_user_agent()}
@@ -55,11 +60,10 @@ def check_price(amazon_item):
 
 
 def send_email(amazon_item, price):
-
-    # Email sending function
-    # @name = name of the item
-    # @price = price of the item
-
+    """ Function that sends email
+    amazon_item = name of the item from the AmazonItem class
+    price = actual price of the item
+    """
     # Trying to read JSON file
     try:
         with open(os.path.join(sys.path[0], 'settings.json')) as json_file:
@@ -68,7 +72,7 @@ def send_email(amazon_item, price):
         print('Cannot read the settings.json file \n', e)
         sys.exit()
 
-    # Email server settings
+    # Email server settings taken from the JSON file
     email_settings = settings['email_settings']
     sender = email_settings[0]['sender']
     recipient = email_settings[0]['recipient']
@@ -77,24 +81,25 @@ def send_email(amazon_item, price):
     smtp_username = email_settings[0]['smtp_username']
     smtp_password = email_settings[0]['smtp_password']
 
+    # Establishing the connection with the smtp server
     server = smtplib.SMTP(smtp_server, smtp_port)
     server.ehlo()
     server.starttls()
     server.ehlo()
     server.login(smtp_username, smtp_password)
 
-    # Body and subject of the email
     subject = 'The price for {} is {}'.format(amazon_item.name, price)
     body = 'Check it now on Amazon at link \n {}'.format(amazon_item.url)
+
+    # Combining the subject and body to make the message
     msg = "Subject: {} \n\n {}".format(subject, body)
 
     server.sendmail(
-        # From
         sender,
-        # To
         recipient,
         msg
     )
+    
     print('Hey! I\'ve just sent the email')
     server.quit()
 
@@ -110,8 +115,8 @@ items_list.append(AmazonItem('Striscia LED Wifi Onforu',
 items_list.append(AmazonItem('Striscia LED Wifi Bawoo',
                              'https://www.amazon.it/Impermeabile-Illuminazione-Bawoo-Assistant-Telecomando/dp/B078SNWRS4', 240))
 
-
-while True:
+# Every 30 seconds I try to check the prices
+while True:    
     try:
         for item in items_list:
             check_price(item)
